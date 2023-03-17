7951529efad3790b425eb654a5a12db2e5402993
MohamedChafiq <medchafiq21@gmail.com>
2023-03-17 14:08:35 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'get the last degiti of a number'
- * Return: always 0
+ *main - Entry point
+ *Description: 'get the last degiti of a number'
+ *Return: always 0
  */
 int main(void)
 {
