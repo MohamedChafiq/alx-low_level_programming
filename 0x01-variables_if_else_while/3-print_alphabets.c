c0ef449693f39cf29f18dff5b0800e352b4b1f46
MohamedChafiq <medchafiq21@gmail.com>
2023-03-18 11:30:42 -0700

    4-puts.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'Print alphabets lowercase'
- * Return: Always 0
+ *main - Entry point
+ *Description: 'Print alphabets lowercase'
+ *Return: Always 0
  */
 int main(void)
 {
 	int n = 97;
 	int m = 65;
-
 	while (n <= 122)
 	{
 		putchar(n);
