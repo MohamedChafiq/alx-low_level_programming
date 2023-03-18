b3d3625c7d36ed48a35c63c0f6a94d7d129f036e
MohamedChafiq <medchafiq21@gmail.com>
2023-03-18 11:57:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
+ * This is the preferred style for multi-line
+ * comments in C source code
+ * Please use it consistently
+ * Description:  A column of asterisks on the left side
+ * with beginning and ending almost-blank lines
  * Return : 0 (succes)
  */
 int main(void)
