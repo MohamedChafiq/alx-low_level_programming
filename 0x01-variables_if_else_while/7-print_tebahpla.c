d652a98371814ac80844f9a1ba63260ad414083c
MohamedChafiq <medchafiq21@gmail.com>
2023-03-20 08:41:58 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'print alphabet in reverse'
- * return: always 0
+ * main - main function
+ *
+ * Return: always 0
  */
 int main(void)
 {
